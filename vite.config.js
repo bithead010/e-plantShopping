a0ce7a0b4dbf828a450f26d87e0c1b0fad8a7f0f
bithead010 <bithead010@gmail.com>
2024-09-19 15:38:31 -0400

    Deployed on github page 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/bithead010/e-plantShopping",
+//   base: "/bithead010/e-plantShopping",
+  base: "/e-plantShopping",
   plugins: [react()],
 })
